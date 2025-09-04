d9d4dad68dd1c23fd0903e6a1f8ec234a95fb073
Banyu <legensbanyu@gmail.com>
2025-09-04 08:54:47 +0700

    SETUP [NYUB] setup pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list_app/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const LoginPage(),
     );
   }
 }
---
+++ lib/pages/history_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HistoryPages extends StatelessWidget {
+  const HistoryPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
